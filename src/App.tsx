4f1088bbe51ca2193698b047c7b063e240245b5a
Dawid <daw.tomczyk1997@gmail.com>
2022-07-24 13:41:49 +0200

    fetch plugins file and caching for better performance

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import * as esbuild from "esbuild-wasm";
 import { useEffect, useState, useRef } from "react";
+import { fetchPlugin } from "./plugins/fetch-plugin";
 import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
 
 export const App = () => {
@@ -7,29 +8,28 @@ export const App = () => {
   const [code, setCode] = useState("");
   const ref = useRef<any>();
 
-  useEffect(()=>{
-   startService();
-  },[])
+  useEffect(() => {
+    startService();
+  }, []);
 
   const startService = async () => {
     ref.current = await esbuild.startService({
       worker: true,
-      wasmURL: "/esbuild.wasm",
+      wasmURL: "https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm",
     });
-    
   };
 
   const handleClick = async () => {
-    if(!ref.current) return;
+    if (!ref.current) return;
     const result = await ref.current.build({
-        entryPoints: ['index.js'],
-        bundle:true,
-        write:false,
-        plugins: [unpkgPathPlugin()],
-        define: {
-          'process.env.NODE_ENV': '"production"',
-          global: 'window'
-        }
+      entryPoints: ["index.js"],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
+      define: {
+        "process.env.NODE_ENV": '"production"',
+        global: "window",
+      },
     });
 
     //console.log(result);
--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,47 +1,27 @@
-import axios from 'axios';
 import * as esbuild from 'esbuild-wasm';
+
 
 export const unpkgPathPlugin = () => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
-      build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log('onResolve', args);
-        if (args.path === 'index.js') {
-          return { path: args.path, namespace: 'a' };
-        }
 
-        if(args.path.includes('./') || args.path.includes('../')) {
-          return {
-            namespace:'a',
-            path: new URL(args.path, 'https://unpkg.com' + args.resolveDir + '/').href
-          };
-        }
-
-        return {
-          namespace:'a',
-          path:`https://unpkg.com/${args.path}`
-        }
+      // Handle root entry file of index.js
+      build.onResolve({ filter: /(^index\.js$)/ }, () => {
+        return { path: 'index.js', namespace: 'a' }
       });
-
-      build.onLoad({ filter: /.*/ }, async (args: any) => {
-        console.log('onLoad', args);
-
-        if (args.path === 'index.js') {
-          return {
-            loader: 'jsx',
-            contents: `
-              import React from 'react@16.0.0';
-              console.log(React);
-            `,
-          };
-        }
-        const {data,request} = await axios.get(args.path);
-        console.log(request)
+      // Handle relative paths in a module
+      build.onResolve({ filter: /^\.+\// }, (args: any) => {
         return {
-          loader:'jsx',
-          contents:data,
-          resolveDir: new URL('./', request.responseURL).pathname
+          namespace: 'a',
+          path: new URL(args.path, 'https://unpkg.com' + args.resolveDir + '/').href
+        };
+      })
+      // Handle main file of a module
+      build.onResolve({ filter: /.*/ }, async (args: any) => {
+        return {
+          namespace: 'a',
+          path: `https://unpkg.com/${args.path}`
         }
       });
     },
