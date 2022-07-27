bf7d853b7928bdc67b5816e88e5ae321bbd45a52
Dawid <daw.tomczyk1997@gmail.com>
2022-07-27 19:46:08 +0200

    markdown-editor implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { CodeCell } from "./components/CodeCell";
+import { TextEditor } from "./components/TextEditor";
 
 export const App = () => {
   return (
     <div>
       <CodeCell/>
+      <TextEditor/>
     </div>
   );
 };
